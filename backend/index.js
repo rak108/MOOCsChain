bc0d1a599384838a474ff3b3262c8d35c21e51c2
Rakshita Varadarajan <rakshitajps@gmail.com>
2022-10-09 13:37:04 +0530

    added RA Logic

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,20 @@
+'use strict';
+
 const express = require('express');
+const crypto = require('crypto')
+
 const { create } = require('ipfs-http-client');
 const axios = require('axios');
 
 const ipfs = create('http://localhost:5001');
 const app = express();
 
+// const testContract = require('MOOCsChain/backend/chaincode');
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    return res.send('MOOCsChain Application -> Current progress only IPFS');
+    return res.send('MOOCsChain Application');
 });
 
 app.post('/uploadElr', async (req, res) => {
@@ -43,3 +49,55 @@ const retrieveELR = async (elrStoredHash) => {
 app.listen(3000, () => {
     console.log('----Server is up and running on port 3000----');
 });
+
+const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 2048,
+  });
+
+const P = 11;
+  
+const registerEntity = async ( info ) => {
+    const encryptedData = crypto.publicEncrypt(
+        {
+          key: publicKey,
+          padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+          oaepHash: "sha256",
+        },
+        Buffer.from(info)
+      );
+    
+    sigma = encryptedData * P;
+    const privKey = crypto.createHmac("sha256", (encryptedData | publicKey));
+    PubKey = privKey * P;
+    registrationTime = new Date().toISOString()
+    // Call Blockchain
+    return 1;
+}
+
+const saveELRs = async ({ sigma, PubKey, course_id, elrContent }) => {
+    const encryptedData = crypto.publicEncrypt(
+        {
+          key: publicKey,
+          padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+          oaepHash: "sha256",
+        },
+        Buffer.from(elrContent)
+      );
+    
+    const hashValue = crypto.createHmac("sha256", elrContent);
+
+    const elrIPFS = uploadELR(hashValue, encryptedData);
+    // Call Blockchain
+    return 1;
+}
+
+const getELRs = async ({ sigma, course_id }) => {
+
+    const hashValue = crypto.createHmac("sha256", elrContent);
+
+    const elrIPFS = uploadELR(hashValue, encryptedData);
+    // Call Blockchain
+    return 1;
+}
+
+// module.exports.contracts = [ testContract ];
